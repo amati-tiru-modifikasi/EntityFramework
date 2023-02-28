266e1cecef06bc1cc6c3d4fba9dbc36d937961db
mdestafadilah <desta.08b@gmail.com>
2023-02-28 07:05:18 +0700

    UPDATE - INSTALL SQL SERVER - RUN 'dotnet ef migrations add InitialCreate'

--- Data/EntitiFrameworkContext.cs
+++ Data/EntitiFrameworkContext.cs
@@ -1,13 +1,14 @@
 using EntitiFramework.Models;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace EntitiFramework.Data;
 
 public class EntitiFrameworkContext : DbContext
 {
-    public DbSet<Customer> Customers {get; set;} = null;
+    public DbSet<Customers> Customers {get; set;} = null;
     public DbSet<Order> Orders {get; set;} = null;
-    public DbSet<Product> Products {get; set;} = null;
+    public DbSet<Products> Products {get; set;} = null;
     public DbSet<OrderDetail> OrderDetails {get; set;} = null;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
