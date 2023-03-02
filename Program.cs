d7196864881d7f83c4739eb4ac69ede715af94f1
mdestafadilah <desta.08b@gmail.com>
2023-03-02 21:00:01 +0700

    change base project

--- Program.cs
+++ Program.cs
@@ -1,23 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿using EntitiFramework.Data;
+using EntitiFramework.Models;
 
-// Add services to the container.
+using EntitiFrameworkContext context = new EntitiFrameworkContext();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+Products veggieSpecial = new Products() {
+    Name = "Veggie Special Pizza",
+    Price = 9.99M
+};
+context.Products.Add(veggieSpecial);
 
-var app = builder.Build();
+Products deluxeMeat = new Products() {
+    Name = "Deluxe Meat Pizza",
+    Price = 12.99M
+};
+context.Add(deluxeMeat);
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+context.SaveChanges();
