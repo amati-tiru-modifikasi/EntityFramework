282e2a52d42a09cacf87d4d136d2e88459733003
mdestafadilah <desta.08b@gmail.com>
2023-03-03 04:26:45 +0700

    check if null

--- Program.cs
+++ Program.cs
@@ -3,6 +3,18 @@ using EntitiFramework.Models;
 
 using EntitiFrameworkContext context = new EntitiFrameworkContext();
 
+var veggiSpecial = context.Products
+                          .Where(p => p.Name == "Veggie Special Pizza")
+                          .FirstOrDefault();
+
+// if null
+if(veggiSpecial is Products) {
+    veggiSpecial.Price = 10.99M;
+}
+
+// persist to db
+context.SaveChanges();
+
 /* LINK SYNTAK API ~ SQL STYLE */
 var productss = from products in context.Products
                 where products.Price > 10.00M
