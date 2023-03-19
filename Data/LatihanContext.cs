e83607933a892e5d81a794ea19dd28ae26d73d97
mdestafadilah <desta.08b@gmail.com>
2023-03-19 21:30:40 +0700

    update part 2

---
+++ Data/LatihanContext.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using EntitiFramework.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntitiFramework.Data;
+
+public partial class LatihanContext : DbContext
+{
+    public LatihanContext()
+    {
+    }
+
+    public LatihanContext(DbContextOptions<LatihanContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=HP-PROBOOK;Initial Catalog=latihan;Integrated Security=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("Department");
+
+            entity.Property(e => e.DepartmentId).ValueGeneratedOnAdd();
+            entity.Property(e => e.DepartmentName).HasMaxLength(500);
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("Employee");
+
+            entity.Property(e => e.DateOfJoining).HasColumnType("datetime");
+            entity.Property(e => e.Department).HasMaxLength(500);
+            entity.Property(e => e.EmployeeId).ValueGeneratedOnAdd();
+            entity.Property(e => e.EmployeeName).HasMaxLength(500);
+            entity.Property(e => e.PhotoFileName).HasMaxLength(500);
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasIndex(e => e.CustomerId, "IX_Orders_CustomerId");
+
+            entity.HasOne(d => d.Customer).WithMany(p => p.Orders).HasForeignKey(d => d.CustomerId);
+        });
+
+        modelBuilder.Entity<OrderDetail>(entity =>
+        {
+            entity.HasIndex(e => e.OrderId, "IX_OrderDetails_OrderId");
+
+            entity.HasIndex(e => e.ProductId, "IX_OrderDetails_ProductId");
+
+            entity.HasOne(d => d.Order).WithMany(p => p.OrderDetails).HasForeignKey(d => d.OrderId);
+
+            entity.HasOne(d => d.Product).WithMany(p => p.OrderDetails).HasForeignKey(d => d.ProductId);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Price).HasColumnType("decimal(6, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
